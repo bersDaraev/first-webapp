fb226ecd437953ed749127fec72d09cefcd132a3
bersDaraev <134153202+bersDaraev@users.noreply.github.com>
2023-05-20 19:38:12 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-function popUp() {
-    alert("Call now! 917 794 96 88");
+var contactButtons = document.querySelectorAll('.button');
+
+for (var i = 0; i < contactButtons.length; i++) {
+  contactButtons[i].addEventListener('click', function() {
+    var phoneNumber = "+123456789"; // Replace with your desired phone number
+
+    if (isMobileDevice()) {
+      // Dial the phone number on mobile devices
+      window.location.href = "tel:" + phoneNumber;
+    } else {
+      // Open a pop-up with the phone number on desktop devices
+      alert("Call us: " + phoneNumber);
+    }
+  });
+}
+
+function isMobileDevice() {
+  return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
 }
